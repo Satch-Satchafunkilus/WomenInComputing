41b66a6cd5944e1857f70d3d6c0d467350ce5a08
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-06-13 19:19:04 -0500

    Initial Commit

---
+++ WomenInComputing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WomenInComputing
+//
+//  Created by Tushar Munge on 6/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by Tushar Munge on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
